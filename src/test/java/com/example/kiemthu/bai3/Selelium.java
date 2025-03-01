16954ed347d19ca209bad63aa29ba62bcd1af166
dmdmdmmdmmdm <manh56823@gmail.com>
2025-03-02 03:05:07 +0700

    Selelium comment

--- src/test/java/com/example/kiemthu/bai3/Selelium.java
+++ src/test/java/com/example/kiemthu/bai3/Selelium.java
@@ -13,32 +13,32 @@ import java.time.Duration;
 
 public class Selelium {
 
-    private WebDriver webDriver;
-
-    @BeforeEach
-    void setUp(){
-        webDriver = new ChromeDriver();
-    }
-    @Test
-    void TestWeb() throws InterruptedException {
-
+//    private WebDriver webDriver;
+//
+//    @BeforeEach
+//    void setUp(){
+//        webDriver = new ChromeDriver();
+//    }
+//    @Test
+//    void TestWeb() throws InterruptedException {
+//
+////        webDriver.get("https://vitimex.com.vn/");
+////        //dadnwwng ký
+////        webDriver.findElement(By.xpath("//div[@class='li_log']//a[@data-bs-toggle='modal']//*[name()='svg']//*[name()='path' and contains(@d,'M3 20C5.33')]")).click();
+////        webDriver.get("https://vitimex.com.vn/dang-ky.html");
+////        webDriver.findElement(By.xpath("//input[@id='name']")).sendKeys("manh");
+////        webDriver.findElement(By.xpath("//input[@id='phone']")).sendKeys("0386349304");
+////        webDriver.findElement(By.xpath("//input[@id='email']")).sendKeys("manh56823@gmail.com");
+////        webDriver.findElement(By.xpath("//input[@id='pass-regis']")).sendKeys("manh123");
+////        webDriver.findElement(By.xpath("//input[@id='pass-confirm']")).sendKeys("manh123");
+////        webDriver.findElement(By.xpath("//a[@id='register-btn']")).click();
 //        webDriver.get("https://vitimex.com.vn/");
-//        //dadnwwng ký
+//
 //        webDriver.findElement(By.xpath("//div[@class='li_log']//a[@data-bs-toggle='modal']//*[name()='svg']//*[name()='path' and contains(@d,'M3 20C5.33')]")).click();
-//        webDriver.get("https://vitimex.com.vn/dang-ky.html");
-//        webDriver.findElement(By.xpath("//input[@id='name']")).sendKeys("manh");
-//        webDriver.findElement(By.xpath("//input[@id='phone']")).sendKeys("0386349304");
-//        webDriver.findElement(By.xpath("//input[@id='email']")).sendKeys("manh56823@gmail.com");
-//        webDriver.findElement(By.xpath("//input[@id='pass-regis']")).sendKeys("manh123");
-//        webDriver.findElement(By.xpath("//input[@id='pass-confirm']")).sendKeys("manh123");
-//        webDriver.findElement(By.xpath("//a[@id='register-btn']")).click();
-        webDriver.get("https://vitimex.com.vn/");
-
-        webDriver.findElement(By.xpath("//div[@class='li_log']//a[@data-bs-toggle='modal']//*[name()='svg']//*[name()='path' and contains(@d,'M3 20C5.33')]")).click();
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-        WebElement emailInput = wait.until(ExpectedConditions.elementToBeClickable(By.id("account")));
-        emailInput.sendKeys("manh56823@gmail.com");
-        webDriver.findElement(By.xpath("//input[@id='pass']")).sendKeys("manh123");
-        webDriver.findElement(By.xpath("//a[@id='signin-btn']")).click();
-    }
+//        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+//        WebElement emailInput = wait.until(ExpectedConditions.elementToBeClickable(By.id("account")));
+//        emailInput.sendKeys("manh56823@gmail.com");
+//        webDriver.findElement(By.xpath("//input[@id='pass']")).sendKeys("manh123");
+//        webDriver.findElement(By.xpath("//a[@id='signin-btn']")).click();
+//    }
 }
